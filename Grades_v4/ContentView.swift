6bb9a200c29a1f0fc08da5123ca7316f4c431a69
Etienne Hüttl <etienne.huettl@gmail.com>
2024-02-29 15:55:03 +0100

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- Grades_v4/ContentView.swift
+++ Grades_v4/ContentView.swift
@@ -9,17 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Text("Hello, world!")
-            Text("Hello, world!")
-            Text("Hello")
-            Text("Hallo hier war Etienne")
+        NavigationView{
+            List(){
+                RoundedRectangle(cornerRadius: 29.09)
+                    .frame(width: 320, height: 130)
+                    .foregroundStyle(Color(cgColor: CGColor(red: 0.170677, green: 0.350403, blue: 0.225609, alpha: 1)))
+                    .opacity(0.77)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                RoundedRectangle(cornerRadius: 29.09)
+                    .frame(width: 320, height: 130)
+                    .foregroundStyle(Color(.black))
+                    .opacity(0.77)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+            }.navigationBarItems(trailing: Button(action: {
+                
+            }){
+                Image(systemName: "plus.rectangle.on.folder.fill")
+                    .resizable()
+                    .padding(4)
+                    .frame(width: 50, height: 40)
+
+                    .foregroundColor(.black)
+            } )
+            .navigationTitle("Grades").frame(maxWidth: .infinity, alignment: .leading)
+            
         }
-        .padding()
     }
 }
 
