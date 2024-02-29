16461a392242f759d527e43071e0d74ab50bc1f0
Jakob <jakob.erlebach@icloud.com>
2024-02-29 15:41:11 +0100

    Initial Commit

---
+++ Grades_v4/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Grades_v4
+//
+//  Created by Jakob Erlebach on 29.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Grades_v4/Grades_v4App.swift
@@ -0,0 +1,17 @@
+//
+//  Grades_v4App.swift
+//  Grades_v4
+//
+//  Created by Jakob Erlebach on 29.02.24.
+//
+
+import SwiftUI
+
+@main
+struct Grades_v4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
