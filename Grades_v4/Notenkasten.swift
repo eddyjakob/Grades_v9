33c618cebf9124a942f47ebe7a904eb9d758aa77
Jakob <jakob.erlebach@icloud.com>
2024-03-05 15:49:32 +0100

    Merge remote-tracking branch 'refs/remotes/origin/main'

---
+++ Grades_v4/Notenkasten.swift
@@ -0,0 +1,20 @@
+//
+//  Notenkasten.swift
+//  Grades_v4
+//
+//  Created by Jakob Erlebach on 06.03.24.
+//
+
+import SwiftUI
+
+struct Notenkasten: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Notenkasten_Previews: PreviewProvider {
+    static var previews: some View {
+        Notenkasten()
+    }
+}
