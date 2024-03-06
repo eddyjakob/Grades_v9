51ced43add051c564056c977f3292783345beb26
Hunter482 <161045593+Hunter482@users.noreply.github.com>
2024-03-06 19:36:49 +0100

    Update Notenkasten.swift

--- Grades_v4/Notenkasten.swift
+++ Grades_v4/Notenkasten.swift
@@ -5,16 +5,42 @@
 //  Created by Jakob Erlebach on 06.03.24.
 //
 
+
 import SwiftUI
 
 struct Notenkasten: View {
+    
+    var note: String
+    var testtyp: String
+    var accentcolor: Color = .white
+    var notencolor: Color = .red
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack{
+            RoundedRectangle(cornerRadius: 27.54)
+                .frame(height: 70)
+                .foregroundColor(.black)
+                .opacity(0.65)
+            HStack{
+                
+                Text(testtyp)
+                    .foregroundColor(accentcolor)
+                    .font(.title2)
+                    .bold()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                    .padding(.leading, 20)
+                 
+                Text(note)
+                    .foregroundColor(notencolor)
+                    .font(.title)
+                    .bold()
+                    .padding(.trailing, 20)
+                
+            }
+        }.frame(width: 330)
     }
 }
 
-struct Notenkasten_Previews: PreviewProvider {
-    static var previews: some View {
-        Notenkasten()
-    }
+#Preview {
+    Notenkasten(note: "1,5", testtyp:"1.Klassenarbeit")
 }
